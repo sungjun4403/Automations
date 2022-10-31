eea93648c993da8a717e2a93000a8fc2c89f4471
sungjun4403 <geulligu89@naver.com>
2022-10-31 14:29:34 +0900

    .

--- RequestChecker/credientials.py
+++
@@ -1,5 +0,0 @@
-class Credientials:
-    def __init__(self): 
-        # self.pw = 'zbfq nxgd ohrf crsb' #for ubuntu 
-        self.uid = 'geulligu89@gmail.com'
-        self.pw = 'rqcl quey kybp uulo'
